523489cbe1eaac7cf926860cc393ca03f27a7d0e
John D <jddhalogamer@gmail.com>
2022-03-15 20:32:14 -0500

    added an errorHandling middleware.

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -3,6 +3,10 @@ const getGoals = (req, res) => {
 }
 
 const postGoal = (req, res) => { 
+	if(!req.body.text) {
+		res.status(400)
+		throw new Error('Please add text field.')
+	}
 	res.status(200).json({ message :"Set goal"})
 }
 
